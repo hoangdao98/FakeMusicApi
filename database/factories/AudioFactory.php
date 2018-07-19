aaa9dd5a2eb14adb541e5e44479016111b07d140
Dao Huy Hoang <hoangdhph04904@gmail.com>
2018-07-19 14:20:03 +0700

    Created Model -a

---
+++ database/factories/AudioFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Model\Audio::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,9 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('/albums', 'AlbumController');
+
+Route::group(['prefix'=>'albums'], function(){
+	Route::apiResource('/{album}/audios', 'AudioController');
+});
